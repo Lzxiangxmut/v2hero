49538294d70df5aba5f1ac278269e9dc6ce7cdf4
Lzxiangxmut <33050661+Lzxiangxmut@users.noreply.github.com>
2019-12-03 17:19:36 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
+echo PORT=$PORT
 sed -i "s/Port 22/Port $PORT/g" /etc/ssh/sshd_config
+
+rtty -I heroku -h 120.25.229.106 -p 3033 -a -s -d PEKKA -k 5 -D
 
 while true
 do
-  sleep 600
+  rtty=$(ps -aef | grep -w 'rtty -I' | grep -vE 'PID|ps|grep' 2>/dev/null)
+  echo rtty=$rtty
+  [ -z "$rtty" ] && {
+    echo "rtty -I heroku -h 120.25.229.106 -p 3033 -a -s -d PEKKA -k 5 -D"
+    rtty -I heroku -h 120.25.229.106 -p 3033 -a -s -d PEKKA -k 5 -D
+  }
+  sleep 30
 done
