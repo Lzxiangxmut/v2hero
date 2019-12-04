a473333e2778d5f266a199a9d6c7e9f1b64cbd90
Lzxiangxmut <33050661+Lzxiangxmut@users.noreply.github.com>
2019-12-04 10:32:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,37 @@
 #!/usr/bin/env bash
 
-jupyter-notebook --no-browser --no-mathjax --ip=* --port $PORT notebooks
+UserDir="/"
+AppPort=$PORT
+AppToken="4015bc9ee91e437d90df83fb64fbbe312d9c9f05"
+
+mkdir -p /root/.jupyter/custom
+cat > /root/.jupyter/jupyter_notebook_config.py << EOF
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = $AppPort
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.allow_root = True
+c.NotebookApp.open_browser = False
+c.NotebookApp.trust_xheaders = True
+c.NotebookApp.disable_check_xsrf = True
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.notebook_dir = u"$UserDir"
+c.NotebookApp.tornado_settings = {"headers":{"Content-Security-Policy": "frame-ancestors * self *:$AppPort;"}}
+c.NotebookApp.token = "$AppToekn"
+EOF
+
+echo "### /root/.jupyter/jupyter_notebook_config.py ###"
+cat /root/.jupyter/jupyter_notebook_config.py
+
+	cat > /root/.jupyter/custom/custom.js << 'EOF'
+define(['base/js/namespace'], function(Jupyter){
+	Jupyter._target = '_self';
+});
+EOF
+	chmod 777 -R /root/.jupyter/custom
 
 while true
 do
+  jupyter-notebook --allow-root
   echo "### ps -aef ###"
   ps -aef
   echo "### netstat -anp ###"
