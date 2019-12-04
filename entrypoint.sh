e3071596f92bb3cd636cf78aba335ac1178b51ac
Lzxiangxmut <33050661+Lzxiangxmut@users.noreply.github.com>
2019-12-04 10:01:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,11 @@
-echo PORT=$PORT
+#!/usr/bin/env bash
 
-UserDir="/"
-AppToekn="4015bc9ee91e437d90df83fb64fbbe312d9c9f05"
+pgcontents init -l $DATABASE_URL --no-prompt
 
-mkdir -p /root/.jupyter/custom
-
-cat > /root/.jupyter/jupyter_notebook_config.py << EOF
-c.NotebookApp.ip = '*'
-c.NotebookApp.port = $PORT
-c.NotebookApp.allow_origin = '*'
-c.NotebookApp.allow_root = True
-c.NotebookApp.open_browser = False
-c.NotebookApp.trust_xheaders = True
-c.NotebookApp.disable_check_xsrf = True
-c.NotebookApp.allow_remote_access = True
-c.NotebookApp.notebook_dir = u"$UserDir"
-c.NotebookApp.tornado_settings = {"headers":{"Content-Security-Policy": "frame-ancestors * self *:$PORT;"}}
-c.NotebookApp.token = "$AppToekn"
-EOF
-
-cat > /root/.jupyter/custom/custom.js << 'EOF'
-define(['base/js/namespace'], function(Jupyter){
-	Jupyter._target = '_self';
-});
-EOF
-
-chmod 777 -R /root/.jupyter/custom
-
-cat /root/.jupyter/jupyter_notebook_config.py
-
-jupyter-notebook --allow-root &
+jupyter notebook \
+	--no-browser --no-mathjax --ip=* --port $PORT \
+	--config=jupyterconfig.py \
+	notebooks
 
 while true
 do
