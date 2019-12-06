2cd4f8999a75068c763bc5fcb613196fa66144dc
Lzxiangxmut <33050661+Lzxiangxmut@users.noreply.github.com>
2019-12-06 11:06:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
-UserDir="/"
-AppPort=$PORT
+#UserDir="/"
+#AppPort=$PORT
 #AppPort=8088
-AppToken="8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"
+#AppToken="8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"
 
-mkdir -p /root/.jupyter/custom
-cat > /root/.jupyter/jupyter_notebook_config.py << EOF
-c.NotebookApp.ip = '*'
-c.NotebookApp.port = $AppPort
-c.NotebookApp.allow_origin = '*'
-c.NotebookApp.allow_root = True
-c.NotebookApp.open_browser = False
-c.NotebookApp.trust_xheaders = True
-c.NotebookApp.disable_check_xsrf = True
-c.NotebookApp.allow_remote_access = True
-c.NotebookApp.notebook_dir = u"$UserDir"
-c.NotebookApp.password = 'sha1:d0270e88f33b:212384922803ca9bd49d1fb6c11ab42651ccb358'
-c.NotebookApp.tornado_settings = {"headers":{"Content-Security-Policy": "frame-ancestors * self *:$AppPort;"}}
-c.NotebookApp.token = "$AppToken"
-EOF
+#mkdir -p /root/.jupyter/custom
+#cat > /root/.jupyter/jupyter_notebook_config.py << EOF
+#c.NotebookApp.ip = '*'
+#c.NotebookApp.port = $AppPort
+#c.NotebookApp.allow_origin = '*'
+#c.NotebookApp.allow_root = True
+#c.NotebookApp.open_browser = False
+#c.NotebookApp.trust_xheaders = True
+#c.NotebookApp.disable_check_xsrf = True
+#c.NotebookApp.allow_remote_access = True
+#c.NotebookApp.notebook_dir = u"$UserDir"
+#c.NotebookApp.password = 'sha1:d0270e88f33b:212384922803ca9bd49d1fb6c11ab42651ccb358'
+#c.NotebookApp.tornado_settings = {"headers":{"Content-Security-Policy": "frame-ancestors * self *:$AppPort;"}}
+#c.NotebookApp.token = "$AppToken"
+#EOF
 
 #echo "### /root/.jupyter/jupyter_notebook_config.py ###"
 #cat /root/.jupyter/jupyter_notebook_config.py
 
-cat > /root/.jupyter/custom/custom.js << 'EOF'
-	define(['base/js/namespace'], function(Jupyter){
-	Jupyter._target = '_self';
-});
-EOF
-chmod 777 -R /root/.jupyter/custom
+#cat > /root/.jupyter/custom/custom.js << 'EOF'
+#define(['base/js/namespace'], function(Jupyter){
+#Jupyter._target = '_self';
+#});
+#EOF
+#chmod 777 -R /root/.jupyter/custom
 
 cat > /root/.jupyter/ssh_key << 'EOF'
 -----BEGIN RSA PRIVATE KEY-----
@@ -62,24 +62,24 @@ PaKZOkuWjjaplBUcsNCrwxJnUGLPv9vgdPv7aRFR+9d3GEkPwp8yD6c=
 EOF
 chmod 600 /root/.jupyter/ssh_key
 
-sed -i "s/Port 22/Port $PORT/g" /etc/ssh/sshd_config
-sed -i 's/UsePrivilegeSeparation yes/UsePrivilegeSeparation no/g' /etc/ssh/sshd_config
-sed -i 's/PermitRootLogin prohibit-password/#PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+#sed -i "s/Port 22/Port $PORT/g" /etc/ssh/sshd_config
+#sed -i 's/UsePrivilegeSeparation yes/UsePrivilegeSeparation no/g' /etc/ssh/sshd_config
+#sed -i 's/PermitRootLogin prohibit-password/#PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+#echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 
-cat /etc/passwd
+#cat /etc/passwd
 
-echo "########### /etc/ssh/sshd_config ##############"
-cat /etc/ssh/sshd_config
+#echo "########### /etc/ssh/sshd_config ##############"
+#cat /etc/ssh/sshd_config
 
-/etc/init.d/ssh restart
+#/etc/init.d/ssh restart
 
-/usr/sbin/sshd &
+#/usr/sbin/sshd &
 
-#sed -i "s/listen 80/listen $PORT/g" /usr/local/nginx/conf/nginx.conf
-#/etc/init.d/nginx start
-#/etc/init.d/mysql start
-#/etc/init.d/php-fpm start
+sed -i "s/listen 80/listen $PORT/g" /usr/local/nginx/conf/nginx.conf
+/etc/init.d/nginx start
+/etc/init.d/mysql start
+/etc/init.d/php-fpm start
 
 
 #rm -rf /var/www/html && mv /www.codewasp.cn /var/www/html
@@ -103,6 +103,5 @@ do
   netstat -anp
   echo "###############################"
   ps -aef
-  ssh -i /root/.jupyter/ssh_key -N -o TCPKeepAlive=yes -o ServerAliveInterval=10 -o ServerAliveCountMax=3 -o StrictHostKeyChecking=no -o ConnectTimeout=10 -o ExitOnForwardFailure=yes -R 22202:127.0.0.1:$PORT -p 22 pekka@120.25.229.106 -v
   sleep 600
 done
